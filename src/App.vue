ea50e8227b89368aa82188c90667bc1d341a02c4
guilhermepassos1 <gepassos06@gmail.com>
2023-05-19 12:01:01 -0300

    inicio do projeto

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {livros} from '@/_data/livros.js'
+</script>
+
+<template>
+ <h1>minha livraria</h1> 
+ <div class="listagem-livros">
+ <div class="card-livro" v-for="livro in livros" :key="livro.id">
+  <img :src="livro.img" alt="capa do livro" class="capa-livro" />
+  <p class="titulo-livro">{{ livro.title }} </p>
+  <p class="autor-livro">{{ livro.autor }} </p>
+ <p class="preco-livro">{{ livro.preco }} </p>
+
+{{ livros }}
+</div>
+</div>
+<!-- {{ livros }} -->
+
+</template>
+
+<style scoped>
+img{
+  width: 250px;
+}
+
+</style>
---
+++ src/_data/livros.js
@@ -0,0 +1,40 @@
+export const livros = [{
+    id: 1,
+    title: 'o podereso chefão',
+    autor: 'mario puzo',
+    preco : 49.9,
+    img: 'https://super.abril.com.br/wp-content/uploads/2022/04/SI_438_Abre_PL_site.jpg?quality=90&strip=info&w=1024&h=682&crop=1'
+  
+    },
+    {
+    id: 2,
+    title : 'o podereso chefão',
+    autor : 'mario puzo',
+    preco : 49.9,
+    img: 'https://super.abril.com.br/wp-content/uploads/2022/04/SI_438_Abre_PL_site.jpg?quality=90&strip=info&w=1024&h=682&crop=1'
+  
+    },
+    {
+    id:3,
+    title : 'o podereso chefão',
+    autor : 'mario puzo',
+    preco : 49.9,
+    img: 'https://super.abril.com.br/wp-content/uploads/2022/04/SI_438_Abre_PL_site.jpg?quality=90&strip=info&w=1024&h=682&crop=1'
+  
+    },
+    {
+    id:4,
+    title : 'o podereso chefão',
+    autor : 'mario puzo',
+    preco : 49.9,
+    img: 'https://super.abril.com.br/wp-content/uploads/2022/04/SI_438_Abre_PL_site.jpg?quality=90&strip=info&w=1024&h=682&crop=1'
+  
+    },
+    {
+    id:5,
+    title : 'o podereso chefão',
+    autor : 'mario puzo',
+    preco : 49.9,
+    img: 'https://super.abril.com.br/wp-content/uploads/2022/04/SI_438_Abre_PL_site.jpg?quality=90&strip=info&w=1024&h=682&crop=1'
+  
+    }]
